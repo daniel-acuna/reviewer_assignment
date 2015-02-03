89069f2226065d70a9e48e3ceb5b718d665ced44
titipata <titipat.a@u.northwestern.edu>
2015-02-02 21:07:04 -0600

    Add skeleton html file and css file

--- reviewer_assignment_website/home/views.py
+++ reviewer_assignment_website/home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render_to_response('home/index.html')
