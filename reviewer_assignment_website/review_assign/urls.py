c90fdbe9cae17851cf5f87bd9faf93dbc6e5637f
titipata <titipat.a@u.northwestern.edu>
2015-02-04 13:00:49 -0600

    Add blank result page for assignment

--- reviewer_assignment_website/review_assign/urls.py
+++ reviewer_assignment_website/review_assign/urls.py
@@ -3,5 +3,6 @@ from review_assign import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^result$', views.result, name='index'),
     url(r'^ajax-upload$', views.import_uploader, name="my_ajax_upload"),
 )
--- reviewer_assignment_website/review_assign/views.py
+++ reviewer_assignment_website/review_assign/views.py
@@ -10,4 +10,9 @@ def index(request):
     return render_to_response('review_assign/index.html',
         {'csrf_token': csrf_token}, context_instance = RequestContext(request))
 
+def result(request):
+    csrf_token = get_token(request)
+    return render_to_response('review_assign/result.html',
+        {'csrf_token': csrf_token}, context_instance = RequestContext(request))
+
 import_uploader = AjaxFileUploader()
