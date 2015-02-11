d525632e95562bfb4ea7fa9551968248ab0a4d48
Daniel Acuna <daniel.acuna@northwestern.edu>
2015-02-11 17:19:51 -0600

    removed opencv dependency

--- reviewer_assignment_website/paper_reviewer_matcher/analyze_caption_topic.py
+++ reviewer_assignment_website/paper_reviewer_matcher/analyze_caption_topic.py
@@ -3,7 +3,6 @@ __author__ = 'titipat'
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import cv2
 import gensim
 from gensim import corpora, models
 import gensim.parsing
@@ -17,7 +16,7 @@ def documents_to_texts(docs):
     texts = []
     for j in range(len(docs)):
         texts.append(gensim.parsing.preprocess_string(docs[j], filters=[lambda x: x.lower(),
-                                                                        unidecode,            
+                                                                        unidecode,
                                                                         gensim.parsing.strip_tags,
                                                                         gensim.parsing.strip_punctuation,
                                                                         gensim.parsing.strip_multiple_whitespaces,
@@ -40,50 +39,3 @@ def compute_word_dist(lda, corpus):
     w_max = [dictionary.id2token[i] for i in np.fliplr([np.argsort(t_distn.dot(tw_distn))]).flatten()[0:10]]
 
     return pd.DataFrame(np.vstack([w_max, p_w]).T)
-
-
-# #### CODE ####
-
-# data = pd.read_csv('image_captions.csv')                        # load caption document data
-# data = data.rename(columns={data.columns[0]: 'id'})             # rename column
-# data_train, data_cv = cross_validation.train_test_split(data, test_size=0.15)  # split to training and testing
-# data_train = pd.DataFrame(data_train, columns=list(data.columns)).fillna('')
-# data_cv = pd.DataFrame(data_cv, columns=list(data.columns)).fillna('')
-
-# captions = list(data_train.img_title + ' ' + data_train.img_caption)  # get all captions
-# texts = documents_to_texts(captions)                            # create list of text from document
-
-# dictionary = corpora.Dictionary(texts)                          # generate dictionary
-# dictionary.filter_extremes(no_below=2, no_above=0.4)            # filter dictionary
-# dictionary.compactify()                                         # compact dictionary
-
-# corpus_train = [dictionary.doc2bow(text) for text in texts]     # get training corpus
-# tfidf_train = models.TfidfModel(corpus_train)                   # generate tfidf model from existing corpus
-
-# # train lda using term-frequency-idf or corpus itself
-# lda = gensim.models.ldamodel.LdaModel(corpus=corpus_train, id2word=dictionary,
-#                                       num_topics=300, alpha='auto',
-#                                       iterations=2000,
-#                                       update_every=1, chunksize=2000,
-#                                       passes=10)
-
-#### DISPLAY TESTING SET ####
-
-def display_lda(index):
-    preds = pd.DataFrame(lda[corpus_cv[index]], columns=['num_topic', 'value']).sort(['value'], ascending=False)
-    preds = list(preds.num_topic[0:5])
-
-    print captions_cv[index], '\n'
-    print texts_cv[index], '\n'
-    print 'Ten words with highest probability: \n', compute_word_dist(lda, corpus_cv[index]), '\n'
-    print preds[0:5], '\n'
-    print 'Selected topic: ', preds
-    for pred in preds:
-        print lda.print_topic(pred, topn=8)
-
-    img = cv2.imread(data_cv.dir[index])
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
-
-# example display
